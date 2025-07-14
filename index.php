dab18e936f8f9b1601ee955fbc52763931a6d6bc
sampathsss <sampath@zerofence.com>
2025-07-14 21:28:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,11 +11,6 @@
         <form action="login.php" method="POST">
             <input type="text" name="username" placeholder="Username" required><br>
             <input type="password" name="password" placeholder="Password" required><br>
-            <select name="role" required>
-                <option value="">Select Role</option>
-                <option value="voter">Voter</option>
-                <option value="candidate">Candidate</option>
-            </select><br>
             <button type="submit">Login</button>
         </form>
         <?php
