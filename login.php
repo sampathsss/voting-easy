f1277d8116b035a34697fd36d8f4e15220802a73
sampathsss <sampath@zerofence.com>
2025-07-14 19:57:23 +0530

    Update login.php

--- login.php
+++ login.php
@@ -23,7 +23,7 @@ if (isset($_POST['login'])) {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Login - Voting System</title>
+    <title>Login - </title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="style.css">
 </head>
