66f7c44a0f6a5e40c4a37f677b3db8094cf54e4c
sampathsss <sampath@zerofence.com>
2025-07-14 19:42:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,7 @@
     <title>PHP voting system </title>
 
     <!-- bootstrap link -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" 
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link <link rel="stylesheet" href="style.css">
 
 </head>
 <body class="bg-dark">
