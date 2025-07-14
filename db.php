0e1f10eb17f49b5a8233382d5b0d5c970af2d506
sampathsss <sampath@zerofence.com>
2025-07-14 21:02:45 +0530

    Update db.php

--- db.php
+++ db.php
@@ -5,7 +5,6 @@ $password = "ueh9xCfYYq";
 $database = "sql12789971";
 
 $conn = new mysqli($host, $user, $password, $database);
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
