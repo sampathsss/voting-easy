307853a9f7e6a834b6a6afa18edc67467b661e86
sampathsss <sampath@zerofence.com>
2025-07-14 20:07:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP voting system </title>
 
     <!-- bootstrap link -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" 
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 </head>
 <body class="bg-dark">
+    <h1 class= "text-info text-center p-3">Voting System</h1>
     <div class="bg-info py-4">
         <h2 class="text-center">Login</h2>
         <div class="container text-center"> 
